17542d528897169108013646cc6c25c356d90913
LifeShort <416065032@qq.com>
2021-05-17 09:05:36 +0800

    全局异常处理

--- src/main/java/com/example/demo/modules/test/controller/Index.java
+++ src/main/java/com/example/demo/modules/test/controller/Index.java
@@ -1,8 +1,7 @@
 package com.example.demo.modules.test.controller;
 
 import com.example.demo.modules.test.dto.request.TestRequestDTO;
-import com.example.demo.modules.test.dto.response.TestResponseDTO;
-import com.example.demo.utils.ResultUtil;
+import com.example.demo.config.global_exception.ResultUtil;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -23,10 +22,7 @@ public class Index {
 
     @GetMapping("/index")
     @ApiOperation("测试接口1")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "test", value = "关键字", required = true, paramType = "query", dataType = "String"),
-    })
-    public ResultUtil<Object> index(@Valid @RequestParam(required = true) String test){
+    public ResultUtil index(@ApiParam(name = "test", value = "订单id", required = true) @RequestParam("test") String test){
         log.info(test);
         return ResultUtil.success(test);
     }
@@ -34,11 +30,14 @@ public class Index {
 
     @PostMapping("/test")
     @ApiOperation(value = "测试接口2",response = TestRequestDTO.class)
-    public ResultUtil<Object> test(@RequestBody TestRequestDTO testRequestDTO){
+    public ResultUtil test(@RequestBody @Valid TestRequestDTO testRequestDTO){
         log.info(testRequestDTO.toString());
-        TestResponseDTO testResponseDTO = new TestResponseDTO();
-        testResponseDTO.setResult(testRequestDTO);
-        return ResultUtil.success(testResponseDTO);
+         Integer num = testRequestDTO.getNum();
+         Integer i = 2;
+         if( i > num){
+             testRequestDTO.setNum(12);
+         }
+        return ResultUtil.success(testRequestDTO);
     }
 
 }
