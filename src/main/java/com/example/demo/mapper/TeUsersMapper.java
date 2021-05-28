991c70351cc3a0d3c313a29c5a736391e0e3af10
LifeShort <416065032@qq.com>
2021-05-28 14:55:38 +0800

    mysql 异常

--- src/main/java/com/example/demo/mapper/TeUsersMapper.java
+++ src/main/java/com/example/demo/mapper/TeUsersMapper.java
@@ -25,7 +25,7 @@ public interface TeUsersMapper extends BaseMapper<TeUsers> {
     @Insert(" INSERT  INTO  te_users ( user_id, name, nickname, mobile, password, token, integral, balance, create_time) VALUES (#{userId}, #{name}, #{nickname}, #{mobile}, #{password}, #{token}, #{integral}, #{balance}, #{createTime}) ")
     int insertUser(TeUsers user);
 
-    @Update("update user set name =#{name},nickname =#{nickname},mobile =#{mobile},password =#{password} where id=#{userId}")
+    @Update("update te_user set name =#{name},nickname =#{nickname},mobile =#{mobile},password =#{password} where id=#{userId}")
     TeUsers updateUser(TeUsers user);
 
 }
